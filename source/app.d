d0511cd5f6e545d4f9193842e251bf3dbb5b70ab
Marie-Joseph <none@ya.biz>
2021-07-01 14:55:42 -0500

    Further simplify

--- source/app.d
+++ source/app.d
@@ -1,27 +1,25 @@
 import core.time : Duration, dur;
-import std.algorithm : fold, map;
 import std.array : split;
 import std.conv : to;
 import std.datetime.date : TimeOfDay;
-import std.file : readText;
-import std.stdio : File, stdin, stdout, writeln;
-import std.string : splitLines;
+import std.stdio : File, stderr, stdin, stdout, writeln;
 
 void main(string[] args) {
-    File fptr = args.length == 2 ? File(args[1], "r") : stdin;
-    Duration acc;
-    foreach (line; fptr.byLine) {
-        string[2] splitLine = line.to!string.split("-");
-        acc += TimeOfDay.fromISOExtString(splitLine[1] ~ ":00") -
-                 TimeOfDay.fromISOExtString(splitLine[0] ~ ":00");
+    try {
+        File fptr = args.length == 2 ? File(args[1], "r") : stdin;
+        Duration acc;
+        foreach (line; fptr.byLine) {
+            string[2] splitLine = line.to!string.split("-");
+            acc += TimeOfDay.fromISOExtString(splitLine[1] ~ ":00") -
+                   TimeOfDay.fromISOExtString(splitLine[0] ~ ":00");
+        }
+        long hours = acc.total!"hours";
+        long minutes = (acc - dur!"hours"(hours)).total!"minutes";
+        stdout.writeln(hours.to!string,
+                       hours == 1 ? " hour, " : " hours, ",
+                       minutes.to!string,
+                       minutes == 1 ? " minute" : " minutes");
+    } catch (Exception e) {
+        stderr.writeln("count-hours: ", e.msg);
     }
-    long hours = acc.total!"hours";
-    long minutes = (acc - dur!"hours"(hours)).total!"minutes";
-    string toPrint = hours == 1 ?
-                     hours.to!string ~ " hour, " :
-                     hours.to!string ~ " hours, ";
-    toPrint ~= minutes == 1 ?
-               minutes.to!string ~ " minute" :
-               minutes.to!string ~ " minutes";
-    stdout.writeln(toPrint);
 }
